876c42695ee78548c64ae44dfe8592626e0e975d
Akash1975 <akashdhaigude1975@gmail.com>
2026-02-03 20:00:55 +0530

    File added success

--- hospital/settings.py
+++ hospital/settings.py
@@ -200,7 +200,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # =======================
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    ".onrender.com",  # <-- your actual Render domain
+]
 
 # =======================
 # Applications
