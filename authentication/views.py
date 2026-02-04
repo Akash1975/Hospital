8ccaa49a4a4454bfb4ffb07b6fbb5512774f35d5
Akash1975 <akashdhaigude1975@gmail.com>
2026-02-04 10:32:42 +0530

    ready for forggot password

--- authentication/views.py
+++ authentication/views.py
@@ -10,26 +10,30 @@ from .forms import RegisterForm
 from .models import PasswordResetOTP
 
 import random
-
-# ================= SENDGRID EMAIL =================
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
 
 def send_email(subject, message, to_email):
-    if not settings.SENDGRID_API_KEY:
-        print("SendGrid key missing")
-        return
+    try:
+        if not settings.SENDGRID_API_KEY:
+            print("SendGrid key missing")
+            return
 
-    email = Mail(
-        from_email=settings.DEFAULT_FROM_EMAIL,
-        to_emails=to_email,
-        subject=subject,
-        plain_text_content=message,
-    )
+        email = Mail(
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            to_emails=to_email,
+            subject=subject,
+            plain_text_content=message,
+        )
 
-    sg = SendGridAPIClient(settings.SENDGRID_API_KEY)
-    sg.send(email)
+        sg = SendGridAPIClient(settings.SENDGRID_API_KEY)
+        sg.send(email)
+
+    except Exception as e:
+        # NEVER crash production for email failure
+        print("SendGrid error:", e)
+
 
 
 
--- hospital/settings.py
+++ hospital/settings.py
@@ -168,13 +168,7 @@ if DEBUG:
     DEFAULT_FROM_EMAIL = "akashdhaigude1907@gmail.com"
 
 else:
-    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-    EMAIL_HOST = "smtp.sendgrid.net"
-    EMAIL_PORT = 587
-    EMAIL_USE_TLS = True
-
-    EMAIL_HOST_USER = "apikey"
-    EMAIL_HOST_PASSWORD = os.environ.get("SENDGRID_API_KEY")
+    SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
 
     DEFAULT_FROM_EMAIL = os.environ.get(
         "DEFAULT_FROM_EMAIL", "no-reply@hospital.com"
