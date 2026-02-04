6deec6d648b3efe159195eacd9cc7848e9a182e4
Akash1975 <akashdhaigude1975@gmail.com>
2026-02-04 16:30:15 +0530

    ready for forggot password

--- hospital/settings.py
+++ hospital/settings.py
@@ -160,18 +160,25 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 import os
 
 # ================= EMAIL (PRODUCTION - SENDGRID) =================
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_USE_TLS = True
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'akashdhaigude1907@gmail.com'
+# EMAIL_HOST_PASSWORD = 'cslwjjrdwnattyuu'
+# DEFAULT_FROM_EMAIL = 'Hospital System <no-reply@akashdhaigude1907.com>'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
+
+# ================= EMAIL SETTINGS =================
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True           # TLS on port 587
 EMAIL_PORT = 587
 
-EMAIL_HOST_USER = 'akashdhaigude1907@gmail.com'
-EMAIL_HOST_PASSWORD = 'krtxyzyhxeegdqda'  # 16-character app password
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")       # Your Gmail address
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")  # 16-char App Password
 
-# Default from email shown in inbox
-DEFAULT_FROM_EMAIL = 'Hospital System <no-reply@akashdhaigude1907.com>'
-
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER  # Must match EMAIL_HOST_USER
 
 
 
