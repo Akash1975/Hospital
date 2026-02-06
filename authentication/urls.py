c1fd4bbdc3780c859c36a6c2bb6894db36526abd
Akash1975 <akashdhaigude1975@gmail.com>
2026-02-06 14:35:59 +0530

    final set up

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,7 +6,4 @@ urlpatterns = [
     path("login/", login_view, name="login"),
     path("", register_view, name="register"),
     path("logout/", logout_view, name="logout"),
-    path("forgot-password/", forgot_password, name="forgot_password"),
-    path("verify-otp/", verify_otp, name="verify_otp"),
-    path("send-otp/", send_otp, name="send_otp"),
 ]
