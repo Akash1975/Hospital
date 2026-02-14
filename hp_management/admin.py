25dfee82ac8c0d7231c08de5c2e0ec9c430da0bf
Akash1975 <akashdhaigude1975@gmail.com>
2026-02-14 16:58:07 +0530

    Appointment_Status updated

--- hp_management/admin.py
+++ hp_management/admin.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
 from .models import Appointment, Doctor, About, Services
 
-
 @admin.register(Services)
 class ServicesAdmin(admin.ModelAdmin):
     list_display = ('service_name',)
 
 @admin.register(About)
-class aboutAdmin(admin.ModelAdmin):
+class AboutAdmin(admin.ModelAdmin):
     list_display = ('about_text',)
 
 @admin.register(Doctor)
@@ -20,22 +19,23 @@ class DoctorAdmin(admin.ModelAdmin):
 class AppointmentAdmin(admin.ModelAdmin):
     list_display = (
         'name', 'email', 'phone', 'date', 'time','doctor',
-        'created_at', 'user', 'message'
+        'created_at', 'user', 'message', 'appointment_status'
     )
-    list_filter = ('date',)
+    list_filter = ('date', 'appointment_status')  # allow filtering by status
     search_fields = ('name', 'email', 'phone')
     ordering = ('-created_at',)
 
-
+    # Make all fields read-only except appointment_status
     readonly_fields = (
         'user', 'name', 'email', 'phone','doctor',
-        'date', 'time', 'message','address', 'created_at','appointment_status'
+        'date', 'time', 'message','address', 'created_at'
     )
 
-
+    # Allow editing only appointment_status
+    fields = readonly_fields + ('appointment_status',)
+    
     def has_add_permission(self, request):
         return False
-
 
     def has_delete_permission(self, request, obj=None):
         return True
